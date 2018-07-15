5f8a9a5c2dc0889da374f1919e4697317e3d6ed2
vlad.zverev <vlad.zverev@gmail.com>
2018-07-16 02:51:11 +0600

    few fixes

--- py_ma/py_ma.py
+++ py_ma/py_ma.py
@@ -1,6 +1,5 @@
 import csv
 import io
-from typing import Dict
 
 from marshmallow import Schema, SchemaOpts
 import xmltodict
@@ -11,16 +10,15 @@ class CSVOptions(SchemaOpts):
         super().__init__(meta, ordered)
         self.dateformat = 'iso'
         self.render_module = csv
-        # self.ordered = True
 
 
 class CSVSchema(Schema):
     OPTIONS_CLASS = CSVOptions
 
-    def loads(self, csv_data: str, many: bool = None, *args, **kwargs) -> Dict:
+    def loads(self, csv_data: str, many: bool = None, *args, **kwargs):
         partial = kwargs.pop('partial', None)
-        f = io.StringIO(csv_data)
-        data = self.opts.render_module.DictReader(f)
+        buf = io.StringIO(csv_data)
+        data = self.opts.render_module.DictReader(buf)
         data = list(data)
         if len(data) == 1 and not many:
             data = data[0]
@@ -32,15 +30,14 @@ class XMLOptions(SchemaOpts):
         super().__init__(*args, **kwargs)
         self.dateformat = 'iso'
         self.render_module = xmltodict
-        # self.ordered = True
 
 
 class XMLSchema(Schema):
     OPTIONS_CLASS = XMLOptions
 
-    def loads(self, csv_data: str, many: bool = None, *args, **kwargs) -> Dict:
+    def loads(self, xml_data: str, many: bool = None, *args, **kwargs):
         partial = kwargs.pop('partial', None)
-        data = self.opts.render_module.parse(csv_data)
+        data = self.opts.render_module.parse(xml_data)
         return self.load(data, many=many, partial=partial)
 
 
@@ -48,7 +45,6 @@ class JSONOptions(SchemaOpts):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.dateformat = 'iso'
-        # self.ordered = True
 
 
 class JSONSchema(Schema):
